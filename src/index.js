abe3ab95ba6d2ff9cef964a61624c6ab64490ad1
Erhannis Kirran <eyeillus@gmail.com>
2020-11-21 04:39:51 -0500

    Further paring

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
-require('../vendor/OrbitControls.js');
 require('./utils.js');
-require('./ui2d.js');
 
 require('./systems/ui.js');
 require('./systems/painter.js');
 
 require('./components/json-model.js');
-require('./components/orbit-controls.js');
 require('./components/paint-controls.js');
 require('./components/ui.js');
 require('./components/ui-raycaster.js');
