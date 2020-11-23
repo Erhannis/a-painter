b351930bbeb2a5f0ee7a39a526791f118f4ce486
Erhannis Kirran <eyeillus@gmail.com>
2020-11-23 07:56:22 -0500

    Oh, fixed the weird crash on hovering

--- src/components/ui.js
+++ src/components/ui.js
@@ -715,13 +715,13 @@ AFRAME.registerComponent('ui', {
     if (!isBrushButton && !isHistory && !isHue) {
       materials.normal = object.material;
       materials.hover = object.material.clone();
-      materials.hover.color = '#FF0000'; //TODO Optionize
+      materials.hover.color = [1,0,0]; //TODO Optionize
       //materials.hover.map = this.system.hoverTexture;
       materials.selected = object.material.clone();
-      materials.selected.color = '#BBBBBB';
+      materials.selected.color = [0.9,0.6,0.6];
       //materials.selected.map = this.system.pressedTexture;
       materials.pressed = object.material.clone();
-      materials.selected.color = '#DDDDDD';
+      materials.pressed.color = [1,0.8,0.8];
       //materials.pressed.map = this.system.pressedTexture;
     }
     this.highlightMaterials[buttonName] = materials;
